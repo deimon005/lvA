479b170d3ad700b8b71719f8ee4ac62eb85ef59f
don-python <134964183+don-python@users.noreply.github.com>
2023-06-22 01:00:55 -0400

    Update 2.0 lanzamiento vertical hacia abajo.py
    cambiamos por completo nuestro programa, agregándole un gráfico en relación al lanzamiento vertical hacia abajo

--- lanzamiento vertical hacia abajo.py
+++ lanzamiento vertical hacia abajo.py
@@ -1,29 +1,37 @@
 import math
-#empezare definiendo las contantes y variables que necesitamos a la hora de calcular un lanzamiento vertical hacia abajo
-#------------------------------------------------------------------------------------------
-#altura inciial debe proporcionarse en metros
-#siento que deberiamos hacer que altura inicial sea una variable input que el usuario puede ingresar e ir desarrollando su problema y que nuestro codigo le de respuesta
-altura_inicial = float(input('ingrese la altura incial desde la cual se este efectuando el lanzamiento vertical hacia abajo(en metros)'))
-#------------------------------------------------------------------------------------------
-#------------------------------------------------------------------------------------------
-#velocidad incial es la velocidad con la que empieza a caer el objeto y se calcula en m/s^2
-velocidad_inicial = float(input('ingrese la velocidad inicial del lanzamiento vertical hacia abajo(en m/s^2)'))
-#------------------------------------------------------------------------------------------
-#------------------------------------------------------------------------------------------
-#la aceleracion por gravedad en este caso es de 9,8 m/s^2
-aceleracion_debido_a_gravedad = 9.8
-#------------------------------------------------------------------------------------------
-#------------------------------------------------------------------------------------------
-#el tiempo que tarda en llegar al suelo
-#se calcula utilizando una formula que utiliza los datos ingresados anteriormente y la libreria math que nos permite realizar calculos matematicos un poco mas complejo
+import matplotlib.pyplot as plt
+# primero definimos las librerias que ocuparemos
+#---------------------------------------------------------------------------------------------------------------------------------------------
+# Creamos una variable en la cual ingresemos la altura de nuestra problematica
+#---------------------------------------------------------------------------------------------------------------------------------------------
+altura_inicial = float(input('Ingrese la altura inicial desde la cual se esta efectuando el lanzamiento vertical hacia abajo (en metros): '))
+#---------------------------------------------------------------------------------------------------------------------------------------------
+# Creamos una variable en la cual ingresemos la velocidad de nuestra problematica
+#---------------------------------------------------------------------------------------------------------------------------------------------
+velocidad_inicial = float(input('Ingrese la velocidad inicial del lanzamiento vertical hacia abajo (en m/s^2): '))
+#---------------------------------------------------------------------------------------------------------------------------------------------
+# Creamos variable que es = a la gravedad 9,8 m/s^2
+#---------------------------------------------------------------------------------------------------------------------------------------------
+aceleracion_debido_a_gravedad = 9.8  # Aceleracion debida a la gravedad en m/s^2
+#---------------------------------------------------------------------------------------------------------------------------------------------
+# Tiempo de vuelo calculado utilizando la formula y la biblioteca math
+#---------------------------------------------------------------------------------------------------------------------------------------------
 tiempo_de_vuelo = math.sqrt((2 * (altura_inicial / aceleracion_debido_a_gravedad)))
-#------------------------------------------------------------------------------------------
-#------------------------------------------------------------------------------------------
-#la veocidad final es la velocidad del objeto antes de tocar el piso
+#---------------------------------------------------------------------------------------------------------------------------------------------
+# Velocidad final alcanzada por el objeto antes de tocar el piso
+#---------------------------------------------------------------------------------------------------------------------------------------------
 velocidad_final = velocidad_inicial + (aceleracion_debido_a_gravedad * tiempo_de_vuelo)
-#------------------------------------------------------------------------------------------
-#{:.2f} esto indicq que el valor se mostrara con 2 digitos despues de el punto
-print("tiempo de vuelo : {:.2f} segundos".format(tiempo_de_vuelo))
-print('la velocidad final alcanzada por el objeto es: {:.2f} m/s^2'.format(velocidad_final))
 
-#wena cabros
+print("Tiempo de vuelo: {:.2f} segundos".format(tiempo_de_vuelo))
+print('La velocidad final alcanzada por el objeto es: {:.2f} m/s^2'.format(velocidad_final))
+
+# Generar los datos para el grafico
+t = [i / 100 for i in range(int(tiempo_de_vuelo * 100) + 1)]  # Intervalo de tiempo en incrementos de 0.01 segundos
+y = [altura_inicial - (0.5 * aceleracion_debido_a_gravedad * tiem ** 2) for tiem in t]  # Altura en funcion del tiempo
+# Graficar los datos
+plt.plot(t, y)
+plt.xlabel('Tiempo (s)')
+plt.ylabel('Altura (m)')
+plt.title('Lanzamiento vertical hacia abajo')
+plt.grid(True)
+plt.show()
