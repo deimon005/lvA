81a32406d813375d97c6e895eec2d0df49114a97
deimon005 <134963531+deimon005@users.noreply.github.com>
2023-06-27 23:30:55 -0400

    revisar
    tkinter update

--- lanzamiento vertical hacia abajo.py
+++ lanzamiento vertical hacia abajo.py
@@ -1,37 +1,66 @@
+import tkinter as tk
 import math
-import matplotlib.pyplot as plt
-# primero definimos las librerias que ocuparemos
-#---------------------------------------------------------------------------------------------------------------------------------------------
-# Creamos una variable en la cual ingresemos la altura de nuestra problematica
-#---------------------------------------------------------------------------------------------------------------------------------------------
-altura_inicial = float(input('Ingrese la altura inicial desde la cual se esta efectuando el lanzamiento vertical hacia abajo (en metros): '))
-#---------------------------------------------------------------------------------------------------------------------------------------------
-# Creamos una variable en la cual ingresemos la velocidad de nuestra problematica
-#---------------------------------------------------------------------------------------------------------------------------------------------
-velocidad_inicial = float(input('Ingrese la velocidad inicial del lanzamiento vertical hacia abajo (en m/s^2): '))
-#---------------------------------------------------------------------------------------------------------------------------------------------
-# Creamos variable que es = a la gravedad 9,8 m/s^2
-#---------------------------------------------------------------------------------------------------------------------------------------------
-aceleracion_debido_a_gravedad = 9.8  # Aceleracion debida a la gravedad en m/s^2
-#---------------------------------------------------------------------------------------------------------------------------------------------
-# Tiempo de vuelo calculado utilizando la formula y la biblioteca math
-#---------------------------------------------------------------------------------------------------------------------------------------------
-tiempo_de_vuelo = math.sqrt((2 * (altura_inicial / aceleracion_debido_a_gravedad)))
-#---------------------------------------------------------------------------------------------------------------------------------------------
-# Velocidad final alcanzada por el objeto antes de tocar el piso
-#---------------------------------------------------------------------------------------------------------------------------------------------
-velocidad_final = velocidad_inicial + (aceleracion_debido_a_gravedad * tiempo_de_vuelo)
 
-print("Tiempo de vuelo: {:.2f} segundos".format(tiempo_de_vuelo))
-print('La velocidad final alcanzada por el objeto es: {:.2f} m/s^2'.format(velocidad_final))
 
-# Generar los datos para el grafico
-t = [i / 100 for i in range(int(tiempo_de_vuelo * 100) + 1)]  # Intervalo de tiempo en incrementos de 0.01 segundos
-y = [altura_inicial - (0.5 * aceleracion_debido_a_gravedad * tiem ** 2) for tiem in t]  # Altura en funcion del tiempo
-# Graficar los datos
-plt.plot(t, y)
-plt.xlabel('Tiempo (s)')
-plt.ylabel('Altura (m)')
-plt.title('Lanzamiento vertical hacia abajo')
-plt.grid(True)
-plt.show()
+def calcular_lanzamiento():
+    try:
+        altura_inicial = float(altura_entry.get())
+        velocidad_inicial = float(velocidad_entry.get())
+        area_explosion = float(area_entry.get())
+        velocidad_piloto = float(velocidad_piloto_entry.get())
+
+        tiempo_impacto = math.sqrt((2 * altura_inicial) / 9.8)
+        velocidad_impacto = velocidad_inicial + 9.8 * tiempo_impacto
+
+        if velocidad_impacto < 0:
+            resultado_label.config(text="La bomba no llega al suelo.")
+        elif tiempo_impacto * velocidad_piloto < area_explosion:
+            resultado_label.config(text="La bomba te alcanzó.")
+        else:
+            resultado_label.config(text="La bomba no te alcanzó.")
+
+        velocidad_final_label.config(text=f"Velocidad final: {velocidad_impacto:.2f} m/s")
+        tiempo_caida_label.config(text=f"Tiempo de caída: {tiempo_impacto:.2f} s")
+
+    except ValueError:
+        resultado_label.config(text="Ingrese valores numéricos válidos.")
+
+
+# Crear la ventana principal
+ventana = tk.Tk()
+ventana.title("Lanzamiento Vertical")
+
+# Etiquetas y campos de entrada
+altura_label = tk.Label(ventana, text="Altura inicial (metros):")
+altura_label.pack()
+altura_entry = tk.Entry(ventana)
+altura_entry.pack()
+
+velocidad_label = tk.Label(ventana, text="Velocidad inicial (m/s):")
+velocidad_label.pack()
+velocidad_entry = tk.Entry(ventana)
+velocidad_entry.pack()
+
+area_label = tk.Label(ventana, text="Área de explosión (metros):")
+area_label.pack()
+area_entry = tk.Entry(ventana)
+area_entry.pack()
+
+velocidad_piloto_label = tk.Label(ventana, text="Velocidad del piloto (m/s):")
+velocidad_piloto_label.pack()
+velocidad_piloto_entry = tk.Entry(ventana)
+velocidad_piloto_entry.pack()
+
+calcular_button = tk.Button(ventana, text="Calcular", command=calcular_lanzamiento)
+calcular_button.pack()
+
+resultado_label = tk.Label(ventana, text="")
+resultado_label.pack()
+
+velocidad_final_label = tk.Label(ventana, text="")
+velocidad_final_label.pack()
+
+tiempo_caida_label = tk.Label(ventana, text="")
+tiempo_caida_label.pack()
+
+ventana.mainloop()
